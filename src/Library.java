807283125720ddc26271a5a2595012833ff1f5a9
Zach Ross <zachr@amazon.com>
2017-10-04 13:28:16 -0700

    Implement Library class

--- src/Library.java
+++ src/Library.java
@@ -5,4 +5,31 @@ public class Library {
 
     private List<LibraryItem> libraryItems;
     private List<Member> members;
+
+    public Library() {}
+
+    public void addItem(LibraryItem item) {
+        libraryItems.add(item);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public LibraryItem getItemByTitle(String title) {
+        for (LibraryItem item : libraryItems) {
+            if (item.getTitle().equals(title)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void checkOutItem(Borrowable item) {
+        item.checkOut();
+    }
+
+    public void checkInItem(Borrowable item) {
+        item.checkIn();
+    }
 }
