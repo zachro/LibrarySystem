f19c2222bc0918339e05fac6f03b05cd9a83471b
Chelsea James <cheljame@amazon.com>
2017-10-04 14:54:03 -0700

    Added JavaDoc comments on class and methods

--- src/Member.java
+++ src/Member.java
@@ -1,6 +1,5 @@
 import java.math.BigDecimal;
 import java.util.List;
-
 /**
  * Defines library member with late fees and all currently checked out items.
  */
@@ -9,7 +8,6 @@ public class Member {
     private String lastName;
     private BigDecimal lateFees;
     private List<Borrowable> checkedOutItems;
-
     /**
      * Creates new Member object.
      * @param firstName Provided Member first name.
@@ -20,25 +18,40 @@ public class Member {
         this.lastName = lastName;
         lateFees = new BigDecimal(0);
     }
-
+    /**
+     * Gets the name of a Member.
+     * @return combined first and last name of the Member.
+     */
     public String getName() {
         return (firstName + " " + lastName);
     }
 
+    /**
+     * Adds late fees to the Member's late fee total.
+     * @param lateFee based on days late and late fee rate for item type.
+     */
     public void addToLateFees(BigDecimal lateFee) {
         lateFees.add(lateFee);
     }
-
+    /**
+     * Gets late fees for a Member.
+     * @return late fees for a Member.
+     */
     public BigDecimal getLateFees() {
         return lateFees;
     }
-
+    /**
+     * Gets items currently checked out by the Member.
+     * @return a Borrowable list of items checked out by the Member.
+     */
     public List<Borrowable> getCheckedOutItems() {
         return checkedOutItems;
     }
-
+    /**
+     * Adds new items to Member's checked out library items.
+     * @param itemToAdd Items to add to the Member's chekced out library items.
+     */
     public void addToCheckedOutItems(Borrowable itemToAdd) {
         checkedOutItems.add(itemToAdd);
     }
-
 }
