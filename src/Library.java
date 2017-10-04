7ad8afb50b94e3ae8bfae1f727caad178a3fc3a8
Zach Ross <zachr@amazon.com>
2017-10-04 14:25:10 -0700

    Implement late fee logic in Library

--- src/Library.java
+++ src/Library.java
@@ -1,8 +1,9 @@
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Library {
-
-
     private List<LibraryItem> libraryItems;
     private List<Member> members;
 
@@ -29,7 +30,28 @@ public class Library {
         item.checkOut();
     }
 
-    public void checkInItem(Borrowable item) {
+    public void checkInItem(Borrowable item, Member member) {
+        int daysCheckedOut = getDaysCheckedOut(item);
+        int gracePeriod = item.getGracePeriodDays();
+
+        // Calculate late fees or item charge, if applicable
+        if (daysCheckedOut > item.getDaysTillCharged()) {
+            BigDecimal fee = item.getPrice();
+            member.addToLateFees(fee);
+        }
+        else if (daysCheckedOut > gracePeriod) {
+            BigDecimal feePerDay = item.getFeePerDay();
+            BigDecimal fee = feePerDay.multiply(new BigDecimal(daysCheckedOut));
+            member.addToLateFees(fee);
+        }
+
         item.checkIn();
+    }
+
+    private int getDaysCheckedOut(Borrowable item) {
+        Date now = new Date();
+        Date checkOutDate = item.getCheckoutDate();
+        long diffMilliseconds = now.getTime() - checkOutDate.getTime();
+        return (int) TimeUnit.DAYS.convert(diffMilliseconds, TimeUnit.MILLISECONDS);
     }
 }
